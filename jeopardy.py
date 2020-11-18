669866b0702adee36e742707fc18ac952f1a6778
jpitman1010 <jpitman1010@gmail.com>
2020-11-17 22:15:09 -0800

    Got background set up, buttons in place, now need to get all working frontend only

--- jeopardy.py
+++ jeopardy.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, flash, session, redirect, url_for
+from flask import Flask, render_template, request, flash, session, redirect, url_for, jsonify, make_response
 import os
 import sys
 from jinja2 import StrictUndefined
@@ -9,18 +9,155 @@ app.secret_key = "dev"
 app.jinja_env.undefined = StrictUndefined
 
 
+question_answer_dict1 = {1: 'Tools of the Writer’s Trade', 2: {
+    200: {'A':"Nabokov’s novels all began on 3×5 these, which he kept under his pillow in case of inspiration", 'Q': 'What is an index card?'},
+    400 : {'A':'In 2009 this, on which Cormac McCarthy produced 5 million words, sold for over $250,000 at auction', 'Q':'What is a typewriter?'},
+    600: {'A':"At London’s Charles Dickens museum, visitors can handle Dickens’ own pen of this type", 'Q': 'What is a fountain pen?'},
+    800: {'A':"Raymond Chandler composed screenplays by talking into this machine, a brand name copyrighted in 1907", 'Q': 'What is a dictaphone?'},
+    1000: {'A':"Steinbeck was obsessed with these; the Mongol 2 3/8 model came closest to the perfect point", 'Q': 'What is a pencil?'}
+    },
+    3: 'Quarterback U.', 4: {
+    200: {'A':"Peyton Manning", 'Q': "What is University of Tennessee?"},
+    400: {'A':"Vince Young", 'Q': "What is University of Texas?"},
+    600: {'A':"Steve Young", 'Q': "What is BYU (Brigham Young)?"},
+    800: {'A':"Drew Brees", 'Q':"What is Purdue?"},
+    1000: {'A':"Jim Kelly & Gino Torretta", 'Q': "What is University of Miami?"}
+    },
+    5: 'The “N” Crowd', 6:{
+    200: {'A':"He was a great rugby & lacrosse player but he’s better known as “The Father of Basketball”", 'Q': 'Who is Naismith?'},
+    400: {'A':"Once in the news himself, today this marine colonel hosts “War Stories” on the Fox News Channel", 'Q': "Who is (Ollie) North?"},
+    600: {'A':"After a breakdown around 1919, this Russian-born ballet dancer spent the rest of his life in & out of asylums", 'Q': "Who is Najinksy?"},
+    800: {'A':"(Daily Double) – He not only won the 1971 Nobel Prize for Literature, he also served in the Chilean senate", 'Q': "Who is (Pablo) Neruda?"},
+    1000: {'A':"Egypt’s president from 1956 to 1970, he dreamed of leading the whole Arab world", 'Q': "Who is Nasser?"}
+    },
+    7: 'Rivers',8: {
+    200: {'A':"As the central river artery, this is one of the world’s busiest waterways", 'Q': "What is the Mississippi?"},
+    400: {'A':"Although its ultimate source is still debated, this river flows into the Atlantic", 'Q': "What is the Amazon?"},
+    600: {'A':"From two small sources, this river flows to the North Sea", 'Q': "What is the Rhine?"},
+    800: {'A':"The head of this river is dry for much of the year and flows to an estuary on the North Sea", 'Q': "What is the Thames?"},
+    1000: {'A':"Flowing westward, this river has a drainage basin that covers seven states", 'Q': "What is the Colorado?"}
+    },
+    9: 'Flaming Foods', 10: {
+    200: {'A': "'Joy of Cooking' instructions for making this cherry dessert: 'Standing back, ignite with a long lighted match'", 'Q': "What is cherries jubilee?"},
+    400: {'A':"Henri Charpentier is said to have created this dessert crepes dish by accident; the dish was a flaming success", 'Q': "What is Crepe Suzette?"},
+    600: {'A':"You can make a flaming version of this tex-mex dish whose name means “little sashes”", 'Q': "What are fajitas?"},
+    800: {'A':"This dessert, cake & ice cream browned in the oven, can be flambeed for a dramatic presentation", 'Q': "What is Baked Alaskan?"},
+    1000: {'A':"Brennan’s in New Orleans says that this flaming fruity dessert is the most requested item on its menu", 'Q': "What is Bananas Foster?"}
+    },
+    11:'Animals in Italian', 12: {
+    200: {'A':"Avoid the sting as you tell us that un’ape is this insect", 'Q': "What is the bee?"},
+    400: {'A':"If you’re called un asino, you’re either a fool, or this animal", 'Q': "What is a donkey?"},
+    600: {'A':"That’s the scariest ragno, one of these, I’ve ever seen! Don’t tell Signorina Muffet", 'Q': "What is a spider?"},
+    800: {'A':"Un cane is this; ready for walkies?", 'Q': "What is a dog"},
+    1000: {'A':"It really gets me that una capra is one of these farm animals", 'Q': "What is a goat?"}
+    }}
+
+question_answer_dict2 = {
+    1: 'The Old West', 2: {
+    400: {'A':"The first of his cowboy hats was called the boss of the plains; Carlsbad, the most popular model, followed", 'Q': "What is Stetson?"},
+    800: {'A':"The act of herding cattle; death was sometimes called “the last” one", 'Q': "What is the roundup?"},
+    1200: {'A':"In 1859 this Nevada town sprung up virtually overnight with the discovery of the Comstock Lode", 'Q': "What is Virginia City?"},
+    1600: {'A':"In 1867 Joseph McCoy set up a shipping yard in Abilene, Kansas to hold Texas cattle arriving on this trail", 'Q': "What is Chisolm Trail"},
+    2000: {'A':"As the result of an injury suffered in his first gunfight in 1876, he began using a cane & often used it as a weapon", 'Q': "Who is Bat Masterson"}
+    },
+    3: 'Books & Movies', 4: {
+    400: {'A':"The Power and the ___ Daze", 'Q': "What is Glory?"},
+    800: {'A':"The Confessions of Nat ___ and Hooch", 'Q': "Who is Turner?"},
+    1200: {'A':"The Painted ___ on a Wire", 'Q': "What is Bird?"},
+    1600: {'A':"Vanity ___ Game.", 'Q':"What is Fair?"},
+    2000: {'A':"The Red and the ___ Knight", 'Q': "What is Black?"}
+    },
+    5:'Julius Caesar Salad',6: {
+    400: {'A':"In 2008 a marble one of these was found in a French river, apparently thrown there right after Caesar’s fall.", 'Q': "What is a bust?"},
+    800: {'A':"It’s named for Julius Caesar & is sometimes written in a date as “7”", 'Q': "What is July?"},
+    1200: {'A':"In Spanish the name becomes this, as on former boxing champion Chavez" , 'Q': "Who is Julio Caesar Chavez?"},
+    1600: {'A':"(Daily Double) – This 5-word line spoken by the soothsayer is a quote from Shakespeare’s “Julius Caesar”", 'Q': "What is Beware the Ides of March?"},
+    2000: {'A':"In 45 B.C. Caesar was given permission to wear this, which he appreciated as it covered his baldness", 'Q': "What is a laurel wreath?"}
+    },
+    7:'Boning Up', 8: {
+    400: {'A':"The name of this condition that affects mostly women literally means “porous bone”", 'Q': "What is osteoporosis?"},
+    800: {'A':"This fatty substance inside the bones is a major site of blood cell production", 'Q': "What is the marrow?"},
+    1200: {'A':"Your false ribs are so called because unlike your true ribs, they aren’t attached to this breastbone", 'Q': "What is the sternum?"},
+    1600: {'A':"(Daily Double) – This bone with a ball-&-socket joint at one end is about 1/4 of your height", 'Q': "What is the femur?"},
+    2000: {'A':"Most broken bones in children are this type of fracture with a colorful name, in which the break cuts only partway through the bone", 'Q': "What is a greenstick?"}
+    },
+    9:'“Big” Stuff', 10: {
+    400: {'A':"Jazz musicians & jockeys helped give New York City this nickname", 'Q': "What is the Big Apple?"},
+    800: {'A':"If you’re under this, you’re at the circus", 'Q': "What is the big top?"},
+    1200: {'A':"Digit also known as the hallux", 'Q': "What is the big toe?"},
+    1600: {'A':"In France this feature of the night sky is known as “le casserole”", 'Q': "What is the Big Dipper?"},
+    2000: {'A':"Rhyming nickname of Boston’s Interstate 93 construction project", 'Q': "What is the Big Dig?"}
+    },
+    11: 'The Sanders of Time', 12: {
+    400: {'A': "A Louisville museum is devoted to the success story of this man who was famous for his breasts, thighs & legs", 'Q': "Who is Col. Sander?"},
+    800: {'A': "In an episode of this Garry Shandling series, the title star thinks David Duchovny is hitting on him", 'Q': "What is The Larry Sanders Show?"},
+    1200: {'A':"This Heisman trophy winner played his college ball at Oklahoma State", 'Q': "Who is Barry Sanders?"},
+    1600: {'A':"Olympic gold medal swimmer and TV personality seen here", 'Q': "Who is Summer Sanders?"},
+    2000: {'A': "On Dec. 10, 2010 he conducted a one-man, 9-hour filibuster on the Senate floor to protest a tax bill", 'Q': "Who is Bernie Sanders?"}
+    }}
+
+category_1 = [question_answer_dict1[2],question_answer_dict1[4],question_answer_dict1[6], question_answer_dict1[8], question_answer_dict1[10], question_answer_dict1[12]]
+category_2 = [question_answer_dict2[2],question_answer_dict2[4],question_answer_dict2[6], question_answer_dict2[8], question_answer_dict2[10], question_answer_dict2[12]]
+categories = question_answer_dict1.keys()
+categories2 = question_answer_dict2.keys()
+answers1 = []
+answers2 = []
+print(categories)
+A1 = []
+Q1 = []
+A2 = []
+Q2 = []
+
+for category in categories:
+    if category %2 != 0:
+        answers1.append(question_answer_dict1[category])
+    else:
+        A1.append(question_answer_dict1[category][200]['A'])
+        Q1.append(question_answer_dict1[category][200]['Q'])
+        A1.append(question_answer_dict1[category][400]['A'])
+        Q1.append(question_answer_dict1[category][400]['Q'])
+        A1.append(question_answer_dict1[category][600]['A'])
+        Q1.append(question_answer_dict1[category][600]['Q'])
+        A1.append(question_answer_dict1[category][800]['A'])
+        Q1.append(question_answer_dict1[category][800]['Q'])
+        A1.append(question_answer_dict1[category][1000]['A'])
+        Q1.append(question_answer_dict1[category][1000]['Q'])
+
+
+for category in categories2:
+    if category %2 != 0:
+        answers2.append(question_answer_dict2[category])
+    else:
+        A1.append(question_answer_dict2[category][400]['A'])
+        Q1.append(question_answer_dict2[category][400]['Q'])
+        A1.append(question_answer_dict2[category][800]['A'])
+        Q1.append(question_answer_dict2[category][800]['Q'])
+        A1.append(question_answer_dict2[category][1200]['A'])
+        Q1.append(question_answer_dict2[category][1200]['Q'])
+        A1.append(question_answer_dict2[category][1600]['A'])
+        Q1.append(question_answer_dict2[category][1600]['Q'])
+        A1.append(question_answer_dict2[category][2000]['A'])
+        Q1.append(question_answer_dict2[category][2000]['Q'])
+
+
+print('categories 1 =', answers1)
+print('categories 2 =', answers2)
+print('answers 1 = ', A1)
+print('questions 1 = ', Q1)
+print('answers 1 = ', A2)
+print('questions 1 = ', Q2)
 
 @app.route('/')
 def game_stage():
-    return render_template("stage.html")
+    return render_template("stage.html",  answers = answers1, A1 = A1, Q1 = Q1)
 
 @app.route('/round_one', methods = ['POST'])
 def round_one():
-    return render_template("round_one.html")
+    return render_template("round_one.html", answers = answers1, A1 = A1, Q1 = Q1)
 
 @app.route('/round_two', methods = ['POST'])
 def round_two():
-    return render_template("round_two.html")
+    return render_template("round_two.html", answers = answers2, A2 = A2, Q2 =Q2)
 
 @app.route('/final_round', methods = ['POST'])
 def final_round():
@@ -30,93 +167,15 @@ def final_round():
 def play_again():
     return render_template("round_one.html")
 
-# def game_play():
+@app.route('/answer', methods = ['POST'])
+def answer():
+    response0 = make_response(jsonify (A1))
+    response1 = make_response(jsonify(Q1))
+    response2 = make_response(jsonify (A2))
+    response3 = make_response(jsonify(Q2))
+    response = [response0, response1, response2, response3]
 
-#     question_answer_dict = {'Tools of the Writer’s Trade': {
-#     200: {"Nabokov’s novels all began on 3×5 these, which he kept under his pillow in case of inspiration": 'What is an index card?'},
-#     400 : {'In 2009 this, on which Cormac McCarthy produced 5 million words, sold for over $250,000 at auction':'What is a typewriter?'},
-#     600: {"At London’s Charles Dickens museum, visitors can handle Dickens’ own pen of this type": 'What is a fountain pen?'},
-#     800: {"Raymond Chandler composed screenplays by talking into this machine, a brand name copyrighted in 1907": 'What is a dictaphone?'},
-#     1000: {"Steinbeck was obsessed with these; the Mongol 2 3/8 model came closest to the perfect point": 'What is a pencil?'}
-#     },
-#     'Quarterback U.': {
-#     200: {"Peyton Manning": "What is University of Tennessee?"},
-#     400: {"Vince Young": "What is University of Texas?"},
-#     600: {"Steve Young": "What is BYU (Brigham Young)?"},
-#     800: {"Drew Brees": "What is Purdue?"},
-#     1000: {"Jim Kelly & Gino Torretta": "What is University of Miami?"}
-#     },
-#     'The “N” Crowd':{
-#     200: {"He was a great rugby & lacrosse player but he’s better known as “The Father of Basketball”": 'Who is Naismith?'},
-#     400: {"Once in the news himself, today this marine colonel hosts “War Stories” on the Fox News Channel": "Who is (Ollie) North?"},
-#     600: {"After a breakdown around 1919, this Russian-born ballet dancer spent the rest of his life in & out of asylums": "Who is Najinksy?"},
-#     800: {"(Daily Double) – He not only won the 1971 Nobel Prize for Literature, he also served in the Chilean senate": "Who is (Pablo) Neruda?"},
-#     1000: {"Egypt’s president from 1956 to 1970, he dreamed of leading the whole Arab world": "Who is Nasser?"}
-#     },
-#     'Rivers': {
-#     200: {"As the central river artery, this is one of the world’s busiest waterways": "What is the Mississippi?"},
-#     400: {"Although its ultimate source is still debated, this river flows into the Atlantic": "What is the Amazon?"},
-#     600: {"From two small sources, this river flows to the North Sea": "What is the Rhine?"},
-#     800: {"The head of this river is dry for much of the year and flows to an estuary on the North Sea": "What is the Thames?"},
-#     1000: {"Flowing westward, this river has a drainage basin that covers seven states": "What is the Colorado?"}
-#     },
-#     'Flaming Foods': {
-#     200: {"“Joy of Cooking” instructions for making this cherry dessert: “Standing back, ignite with a long lighted match": "What is cherries jubilee?"},
-#     400: {"Henri Charpentier is said to have created this dessert crepes dish by accident; the dish was a flaming success": "What is Crepe Suzette?"},
-#     600: {"You can make a flaming version of this tex-mex dish whose name means “little sashes”": "What are fajitas?"},
-#     800: {"This dessert, cake & ice cream browned in the oven, can be flambeed for a dramatic presentation": "What is Baked Alaskan?"},
-#     1000: {"Brennan’s in New Orleans says that this flaming fruity dessert is the most requested item on its menu": "What is Bananas Foster?"}
-#     },
-#     'Animals in Italian': {
-#     200: {"Avoid the sting as you tell us that un’ape is this insect": "What is the bee?"},
-#     400: {"If you’re called un asino, you’re either a fool, or this animal": "What is a donkey?"},
-#     600: {"That’s the scariest ragno, one of these, I’ve ever seen! Don’t tell Signorina Muffet": "What is a spider?"},
-#     800: {"Un cane is this; ready for walkies?": "What is a dog"},
-#     1000: {"It really gets me that una capra is one of these farm animals": "What is a goat?"}
-#     },
-#     'The Old West': {
-#     400: {"The first of his cowboy hats was called the boss of the plains; Carlsbad, the most popular model, followed": "What is Stetson?"},
-#     800: {"The act of herding cattle; death was sometimes called “the last” one": "What is the roundup?"},
-#     1200: {"In 1859 this Nevada town sprung up virtually overnight with the discovery of the Comstock Lode": "What is Virginia City?"},
-#     1600: {"In 1867 Joseph McCoy set up a shipping yard in Abilene, Kansas to hold Texas cattle arriving on this trail": "What is Chisolm Trail"},
-#     2000: {"As the result of an injury suffered in his first gunfight in 1876, he began using a cane & often used it as a weapon": "Who is Bat Masterson"}
-#     },
-#     'Books & Movies': {
-#     400: {"The Power and the ___ Daze": "What is Glory?"},
-#     800: {"The Confessions of Nat ___ and Hooch": "Who is Turner?"},
-#     1200: {"The Painted ___ on a Wire": "What is Bird?"},
-#     1600: {"Vanity ___ Game.":"What is Fair?"},
-#     2000: {"The Red and the ___ Knight": "What is Black?"}
-#     },
-#     'Julius Caesar Salad': {
-#     400: {"In 2008 a marble one of these was found in a French river, apparently thrown there right after Caesar’s fall.": "What is a bust?"},
-#     800: {"It’s named for Julius Caesar & is sometimes written in a date as “7”": "What is July?"},
-#     1200: {"In Spanish the name becomes this, as on former boxing champion Chavez" : "Who is Julio Caesar Chavez?"},
-#     1600: {"(Daily Double) – This 5-word line spoken by the soothsayer is a quote from Shakespeare’s “Julius Caesar”": "What is Beware the Ides of March?"},
-#     2000: {"In 45 B.C. Caesar was given permission to wear this, which he appreciated as it covered his baldness": "What is a laurel wreath?"}
-#     },
-#     'Boning Up': {
-#     400: {"The name of this condition that affects mostly women literally means “porous bone”": "What is osteoporosis?"},
-#     800: {"This fatty substance inside the bones is a major site of blood cell production": "What is the marrow?"},
-#     1200: {"Your false ribs are so called because unlike your true ribs, they aren’t attached to this breastbone": "What is the sternum?"},
-#     1600: {"(Daily Double) – This bone with a ball-&-socket joint at one end is about 1/4 of your height": "What is the femur?"},
-#     2000: {"Most broken bones in children are this type of fracture with a colorful name, in which the break cuts only partway through the bone": "What is a greenstick?"}
-#     },
-#     '“Big” Stuff': {
-#     400: {"Jazz musicians & jockeys helped give New York City this nickname": "What is the Big Apple?"},
-#     800: {"If you’re under this, you’re at the circus": "What is the big top?"},
-#     1200: {"Digit also known as the hallux": "What is the big toe?"},
-#     1600: {"In France this feature of the night sky is known as “le casserole”": "What is the Big Dipper?"},
-#     2000: {"Rhyming nickname of Boston’s Interstate 93 construction project": "What is the Big Dig?"}
-#     },
-#     'The Sanders of Time': {
-#     400: {"A Louisville museum is devoted to the success story of this man who was famous for his breasts, thighs & legs": "Who is Col. Sander?"},
-#     800: {"In an episode of this Garry Shandling series, the title star thinks David Duchovny is hitting on him": "What is The Larry Sanders Show?"},
-#     1200: {"This Heisman trophy winner played his college ball at Oklahoma State": "Who is Barry Sanders?"},
-#     1600: {"Olympic gold medal swimmer and TV personality seen here": "Who is Summer Sanders?"},
-#     2000: {"On Dec. 10, 2010 he conducted a one-man, 9-hour filibuster on the Senate floor to protest a tax bill": "Who is Bernie Sanders?"}
-#     }}
-
+    return jsonify(response=response)
 
 
 
